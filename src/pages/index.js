652cca3138cdbf57f84ab5fd38f1b6cc8ecf4198
natec007 <nate@plaidsystems.com>
2023-04-28 13:55:31 -0700

    Test NDEF

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,13 @@ import IconButton from "../components/IconButton";
 
 function IndexPage({ location, search }) {
   console.log(location.search);
+  let model;
   let deveui;
   let appeui;
   let appkey;
   if (location.search) {
     const params = new URLSearchParams(location.search.match(/\?.*/)[0]);
+    model = params.get("model");
     deveui = params.get("eui");
     appeui = params.get("dev");
     appkey = params.get("key");
@@ -30,6 +32,32 @@ function IndexPage({ location, search }) {
     "name",
     "embarkablelabs"
   );
+
+  async function scan() {
+    console.log("User clicked scan button");
+
+    try {
+      const ndef = new Window.NDEFReader();
+      await ndef.scan();
+      console.log("> Scan started");
+
+      ndef.addEventListener("readingerror", () => {
+        console.log(
+          "Argh! Cannot read data from the NFC tag. Try another one?"
+        );
+      });
+
+      ndef.addEventListener("reading", ({ message, serialNumber }) => {
+        console.log(`> Serial Number: ${serialNumber}`);
+        console.log(`> Records: (${message.records.length})`);
+
+        meow.play();
+      });
+    } catch (error) {
+      console.log("Argh! " + error);
+    }
+  }
+
   return (
     <Layout header={pageData.SEO.title}>
       {/*use default SEOfrom gatsby-config*/}
@@ -40,11 +68,16 @@ function IndexPage({ location, search }) {
           className="absolute object-cover w-full h-full overflow-hidden"
           src={imageLookup.LandingPageImage}
         />
-        <div className="absolute flex flex-col w-full gap-6 p-6 top-1/5">
+        <div className="absolute flex flex-col justify-center w-full gap-6 p-6 mx-auto top-1/5 md:w-160">
           {location.search ? (
             <React.Fragment>
-              <div className="flex flex-col justify-center w-full h-40 p-5 text-center bg-white rounded-md shadow-lg opacity-100">
-                New device found
+              <div
+                onClick={() => {
+                  scan();
+                }}
+                className="flex flex-col justify-center w-full h-40 p-5 text-center bg-white rounded-md shadow-lg opacity-100"
+              >
+                New device found Add to Helium Share keys
               </div>
 
               <div className="flex flex-col justify-center w-full p-5 bg-white rounded-md shadow-lg opacity-100">
