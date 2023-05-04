4530d78a12b55bcda918ca0f0b7b0acbe3d851b6
natec007 <nate@plaidsystems.com>
2023-05-04 15:00:01 -0700

    test write

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,16 @@ import IconButton from "../components/IconButton";
 function IndexPage({ location, search }) {
   console.log(location.search);
 
-  const [NDEFScan, setNDEFScan] = useState([
+  const test = [
     "https://www.device.spruceirrigation.com",
     "6081F942CCE66854",
     "6081F962248666AE",
     "F876C1026FC4695D7BAC5143522A0065",
     "15,15",
     "1,1,1,0,0,1,1,0,0",
-  ]);
+  ];
+
+  const [NDEFScan, setNDEFScan] = useState([]);
 
   let model;
   let deveui;
@@ -118,49 +120,55 @@ function IndexPage({ location, search }) {
           className="absolute object-cover w-full h-full overflow-hidden"
           src={imageLookup.LandingPageImage}
         /> */}
-        <div className="absolute flex flex-col justify-center w-full gap-6 p-6 mx-auto top-1/5 md:w-160">
-          {NDEFScan[0] ? (
+        <div className="absolute flex flex-col justify-center w-full gap-6 p-6 mx-auto md:w-160">
+          {NDEFScan[1] && (
             <React.Fragment>
-              <div className="flex flex-col justify-center w-full h-40 p-5 text-center bg-white rounded-md shadow-lg opacity-100">
-                New device found Add to Helium Share keys
-              </div>
-
               <div className="flex flex-col justify-center w-full p-5 bg-white rounded-md shadow-lg opacity-100">
                 <div>Device Eui {NDEFScan[1]}</div>
                 <div>appeui Eui {NDEFScan[2]}</div>
                 <div>appkey Eui {NDEFScan[3]}</div>
               </div>
             </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <div
-                onClick={() => {
-                  scan();
-                }}
-                className="flex flex-col justify-center w-full h-40 p-5 text-center bg-green-300 rounded-md shadow-lg opacity-100"
-              >
-                Scan device with NFC reader
-              </div>
-              <div
-                onClick={() => {
-                  writeTag();
-                }}
-                className="flex flex-col justify-center w-full h-20 p-5 text-center bg-green-300 rounded-md shadow-lg opacity-100"
-              >
-                Write Tag
-              </div>
-            </React.Fragment>
           )}
+
           <div className="flex flex-col justify-center w-full h-auto gap-3">
-            <div className="flex flex-col justify-center w-full p-2 rounded-md shadow-2xl opacity-100 bg-slate-300">
-              {NDEFScan[0]}
-            </div>
-            {NDEFScan.map((record) => (
+            {NDEFScan[4] && (
+              <div className="flex flex-col justify-center w-full p-2 rounded-md shadow-2xl opacity-100 bg-slate-300">
+                {NDEFScan[4]}
+              </div>
+            )}
+            {NDEFScan[5] && (
+              <div className="flex flex-col justify-center w-full p-2 rounded-md shadow-2xl opacity-100 bg-slate-300">
+                {NDEFScan[5]}
+              </div>
+            )}
+            {/* {NDEFScan.map((record) => (
               <div className="flex flex-col justify-center w-full p-2 rounded-md shadow-2xl opacity-100 bg-slate-300">
                 {record}
               </div>
-            ))}
+            ))} */}
           </div>
+          <React.Fragment>
+            <div
+              onClick={() => {
+                scan();
+              }}
+              className="flex flex-col justify-center w-full h-20 p-5 text-center bg-green-300 rounded-md shadow-lg opacity-100"
+            >
+              Scan device with NFC reader
+            </div>
+            <div
+              onClick={() => {
+                writeTag();
+              }}
+              className="flex flex-col justify-center w-full h-20 p-5 text-center bg-green-300 rounded-md shadow-lg opacity-100"
+            >
+              Write Tag
+            </div>
+            <div className="flex flex-col justify-center w-full h-20 p-5 text-center bg-white rounded-md shadow-lg opacity-100">
+              New device found Add to Helium Share keys
+            </div>
+          </React.Fragment>
           <div className="flex justify-center w-full gap-4 md:h-40">
             <IconButton
               color="white"
