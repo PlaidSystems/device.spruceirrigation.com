38cea9b39f10bd8367622a2209e17fffce25c039
natec007 <nate@plaidsystems.com>
2023-05-16 15:08:05 -0700

    updates ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,15 +15,14 @@ function IndexPage({ location, search }) {
 
   const test = [
     "https://www.device.spruceirrigation.com",
-    "6081F942CCE66854",
-    "6081F962248666AE",
-    "F876C1026FC4695D7BAC5143522A0065",
-    "on",
-    "join",
-    "21.2",
-    "65",
-    "3200",
-    "2100",
+    "DEV1F942CCE66854",
+    "JOINF962248666AE",
+    "APP6C1026FC4695D7BAC5143522A0065",
+    "0",
+    "0",
+    "LORA1-V3",
+    "1515",
+    "21008",
   ];
 
   const [NDEFScan, setNDEFScan] = useState([]);
@@ -152,120 +151,84 @@ function IndexPage({ location, search }) {
 
       <div className="relative h-screen overflow-hidden bg-slate-50">
         {nfcMessage && (
-          <div className="absolute z-10 flex justify-center w-full py-5 bg-red-300 bottom-1/5">
+          <div className="absolute z-10 flex justify-center w-full py-5 bg-red-300 bottom-1/3">
             {nfcMessage}
           </div>
         )}
-        <div className="absolute flex flex-col justify-center w-full gap-6 p-6 mx-auto md:w-160">
-          {NDEFScan[4] !== "on" && (
-            <IconButton
-              color="white"
-              isExternal
-              link="https://spruceirrigation.com/"
-            >
-              <img
-                className="object-cover overflow-hidden rounded-md"
-                src={imageLookup.Spruce}
-              />
-            </IconButton>
-          )}
-
-          {NDEFScan[6] && (
-            <React.Fragment>
-              <div className="flex flex-col justify-center p-5 m-auto bg-white rounded-lg shadow-lg opacity-100 w-80 h-80">
-                <div className="text-center text-green-600 text-7xl">
-                  {NDEFScan[6]}
-                  <span className="text-3xl">%</span>
-                </div>
-                <div className="text-xl text-center">Soil Moisture</div>
+        {NDEFScan[0] && (
+          <React.Fragment>
+            <div className="absolute bottom-0 flex flex-col justify-center w-full gap-6 p-6 mx-auto md:w-160">
+              <div
+                onClick={() => {
+                  writeTag();
+                }}
+                className="flex flex-col self-center justify-center h-20 p-5 mx-auto text-center rounded-md shadow-lg bg-slate-300 w-80"
+              >
+                {NDEFScan[5] === 0 ? (
+                  "Activate Device"
+                ) : (
+                  <span className="text-red-300">De-activate Device</span>
+                )}
               </div>
-            </React.Fragment>
-          )}
-
-          {NDEFScan[7] && (
+              <div
+                onClick={() => {
+                  scan();
+                }}
+                className="flex flex-col self-center justify-center h-20 p-5 mx-auto text-center rounded-md shadow-lg bg-slate-400 w-80"
+              >
+                Scan device
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+        {!NDEFScan[0] && (
+          <div
+            onClick={() => {
+              scan();
+            }}
+            className="flex justify-center p-5 mx-auto mt-40 bg-green-300 rounded-lg shadow-lg w-80 h-80"
+          >
+            <div className="flex self-center text-4xl text-center text-black">
+              Scan device with NFC
+            </div>
+          </div>
+        )}
+        <div className="absolute flex flex-col justify-center w-full gap-6 p-6 mx-auto md:w-160">
+          {NDEFScan[5] && (
             <React.Fragment>
-              <div className="flex flex-col justify-center p-5 m-auto bg-white rounded-lg shadow-lg opacity-100 w-80">
-                <div className="text-center">Temperature {NDEFScan[7]}F</div>
+              <div className="flex flex-col justify-center w-full h-32 p-5 m-auto bg-white rounded-lg shadow-lg opacity-100">
+                <div className="text-3xl text-center text-green-600">
+                  {NDEFScan[6]}
+                </div>
+                <div className="text-center text-md">device name</div>
               </div>
             </React.Fragment>
           )}
 
           {NDEFScan[1] && (
             <React.Fragment>
-              <div className="flex flex-col justify-center w-full p-5 bg-white rounded-md shadow-lg opacity-100">
-                <div>Device Eui {NDEFScan[2]}</div>
-                <div>appeui Eui {NDEFScan[3]}</div>
-                <div>appkey Eui {NDEFScan[4]}</div>
+              <div className="flex flex-col justify-center w-full p-5 text-sm bg-white rounded-md shadow-lg opacity-100">
+                <div className="mt-2 text-xs text-left text-slate-700">
+                  Device Eui
+                </div>
+                <div className="p-2 text-center rounded-sm bg-slate-300 text-md">
+                  {NDEFScan[1]}
+                </div>
+                <div className="mt-2 text-xs text-left text-slate-700">
+                  Join Eui
+                </div>
+                <div className="p-2 text-center rounded-sm bg-slate-300 text-md">
+                  {NDEFScan[2]}
+                </div>
+                <div className="mt-2 text-xs text-left text-slate-700">
+                  App Key
+                </div>
+                <div className="p-2 text-center rounded-sm bg-slate-300 text-md">
+                  {NDEFScan[3]}
+                </div>
               </div>
             </React.Fragment>
           )}
-
-          <div className="flex flex-col justify-center w-full h-auto gap-3">
-            {NDEFScan[7] && (
-              <div className="flex flex-col justify-center w-full p-2 rounded-md shadow-2xl opacity-100 bg-slate-300">
-                {NDEFScan[7]}
-              </div>
-            )}
-            {NDEFScan[8] && (
-              <div className="flex flex-col justify-center w-full p-2 rounded-md shadow-2xl opacity-100 bg-slate-300">
-                {NDEFScan[8]}
-              </div>
-            )}
-            {NDEFScan[9] && (
-              <div className="flex flex-col justify-center w-full p-2 rounded-md shadow-2xl opacity-100 bg-slate-300">
-                {NDEFScan[9]}
-              </div>
-            )}
-          </div>
-          <React.Fragment>
-            <div
-              onClick={() => {
-                scan();
-              }}
-              className="flex flex-col justify-center w-full h-20 p-5 text-center bg-green-300 rounded-md shadow-lg opacity-100"
-            >
-              Scan device with NFC reader
-            </div>
-            <div
-              onClick={() => {
-                writeTag();
-              }}
-              className="flex flex-col justify-center w-full h-20 p-5 text-center bg-green-300 rounded-md shadow-lg opacity-100"
-            >
-              Write Tag
-            </div>
-            <div className="flex flex-col justify-center w-full h-20 p-5 text-center bg-white rounded-md shadow-lg opacity-100">
-              New device found Add to Helium Share keys
-            </div>
-          </React.Fragment>
-          <div className="flex justify-center w-full gap-4 md:h-40">
-            <IconButton
-              color="white"
-              isExternal
-              link="https://spruceirrigation.com/"
-            >
-              <img
-                className="object-cover overflow-hidden rounded-md"
-                src={imageLookup.Spruce}
-              />
-            </IconButton>
-            <IconButton color="white" isExternal link="https://embarkable.io/">
-              <img
-                className="object-cover p-2 overflow-hidden bg-white rounded-md"
-                src={imageLookup.Embarkable}
-              />
-            </IconButton>
-            <IconButton
-              color="white"
-              isExternal
-              link="https://www.linkedin.com/in/nathan-cauffman/"
-            >
-              <img
-                className="object-cover overflow-hidden rounded-md"
-                src={imageLookup.NathanIn}
-              />
-            </IconButton>
-          </div>
         </div>
       </div>
     </Layout>
